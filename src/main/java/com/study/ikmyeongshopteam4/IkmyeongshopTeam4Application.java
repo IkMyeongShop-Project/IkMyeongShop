e1fcb61eecc49cf90e0a1330c75f58b1e0d5f315
Changeun An <eun091696@gmail.com>
2022-10-30 02:13:34 +0900

    Revert "10.29 ADMIN PRODUCT REGISTRATION 화면 생성 진행중"
    This reverts commit 5af151d9bfce00c2f498381ac05720ff3714a353.

--- src/main/java/com/study/ikmyeongshopteam4/IkmyeongshopTeam4Application.java
+++ src/main/java/com/study/ikmyeongshopteam4/IkmyeongshopTeam4Application.java
@@ -2,6 +2,7 @@ package com.study.ikmyeongshopteam4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class IkmyeongshopTeam4Application {
 
--- src/main/java/com/study/ikmyeongshopteam4/exception/CustomInternalServerException.java
+++
@@ -1,9 +0,0 @@
-package com.study.ikmyeongshopteam4.exception;
-
-public class CustomInternalServerException extends RuntimeException {
-
-    public CustomInternalServerException(String message) {
-        super(message);
-    }
-
-}
