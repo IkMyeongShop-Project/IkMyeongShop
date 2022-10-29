f0cb9846e9481e57566800c4e4fdd1ce6bead1d0
Changeun An <eun091696@gmail.com>
2022-10-29 22:11:08 +0900

    10.29 ADMIN PRODUCT REGISTRATION 화면 생성 진행중

--- src/main/java/com/study/ikmyeongshopteam4/IkmyeongshopTeam4Application.java
+++ src/main/java/com/study/ikmyeongshopteam4/IkmyeongshopTeam4Application.java
@@ -2,7 +2,6 @@ package com.study.ikmyeongshopteam4;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class IkmyeongshopTeam4Application {
 
---
+++ src/main/java/com/study/ikmyeongshopteam4/exception/CustomInternalServerException.java
@@ -0,0 +1,9 @@
+package com.study.ikmyeongshopteam4.exception;
+
+public class CustomInternalServerException extends RuntimeException {
+
+    public CustomInternalServerException(String message) {
+        super(message);
+    }
+
+}
