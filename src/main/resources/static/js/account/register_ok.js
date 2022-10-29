14f2e3bb71de871f19e20a9a9467072dc250db53
dhrudgns529 <dhrudgns529@naver.com>
2022-10-29 11:37:46 +0900

    22.10.29 회원가입 완료(thymeleaf로 username 넣기(Controller에 Model의 데이터 담아주기)

--- src/main/resources/static/js/account/register_ok.js
+++ src/main/resources/static/js/account/register_ok.js
@@ -8,8 +8,3 @@ homeButton.onclick = () => {
 loginButton.onclick = () => {
   location.href = "/account/login";
 }
-
-const str = window.location.href.split("=").at(-1);
-
-const textSub = document.querySelector(".text-sub");
-textSub.innerHTML = `<strong>${str}</strong>님의 회원가입을 축하합니다.`;
