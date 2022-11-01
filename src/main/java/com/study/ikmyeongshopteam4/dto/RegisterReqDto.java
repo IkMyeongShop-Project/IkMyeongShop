a43d24b445900efc30bcda242351138b23b333c5
SungHeeCheol <hhhc0213@naver.com>
2022-11-01 22:23:28 +0900

    수정

--- src/main/java/com/study/ikmyeongshopteam4/dto/RegisterReqDto.java
+++ src/main/java/com/study/ikmyeongshopteam4/dto/RegisterReqDto.java
@@ -1,5 +1,6 @@
 package com.study.ikmyeongshopteam4.dto;
 
+import com.study.ikmyeongshopteam4.domain.User;
 import com.study.ikmyeongshopteam4.dto.validation.ValidationGroups;
 import lombok.Data;
 
@@ -7,42 +8,48 @@ import javax.validation.constraints.*;
 
 @Data
 public class RegisterReqDto {
-    @NotBlank(message = "이름은 비워둘 수 없습니다.", groups = ValidationGroups.NotBlankGroup.class)
-    @Size(min = 8, max = 16, message = "아이디는 8~16자여야 합니다.", groups = ValidationGroups.SizeGroup.class)
-    private String username;
+    // 통과 시 사용가능한 아이디입니다.
+    @NotBlank(message = "필수항목 입니다.", groups = ValidationGroups.NotBlankGroup.class)
+    @Size(min = 4, max = 50, message = "최소 4자, 최대 50자 이하 입력해 주세요.", groups = ValidationGroups.SizeGroup.class)
+    private String userName;
 
-    @NotBlank(message = "비밀번호는 비워둘 수 없습니다.", groups = ValidationGroups.NotBlankGroup.class)
-    @Size(min = 8, max = 16, message = "비밀번호는 8자부터 16자까지 입력해야 합니다.", groups = ValidationGroups.SizeGroup.class)
-    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.",
-            groups = ValidationGroups.PatternGroup.class)
+    @NotBlank(message = "필수항목 입니다.", groups = ValidationGroups.NotBlankGroup.class)
+    @Size(min = 10, max = 16, message = "최소 10자, 최대 16자 이하 입력해 주세요.", groups = ValidationGroups.SizeGroup.class)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]*$", message = "비밀번호는 특수기호, 영문, 숫자를 모두 포함하여야 합니다.", groups = ValidationGroups.PatternCheckGroup.class)
     private String password;
 
-    @NotBlank(groups = ValidationGroups.NotBlankGroup.class)
-    @Size(min = 8, max = 16, message = "비밀번호는 8자부터 16자까지 입력해야 합니다.", groups = ValidationGroups.SizeGroup.class)
-    @Pattern(regexp="(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,20}",
-            message = "비밀번호는 영문 대,소문자와 숫자, 특수기호가 적어도 1개 이상씩 포함된 8자 ~ 20자의 비밀번호여야 합니다.",
-            groups = ValidationGroups.PatternGroup.class)
-    private String checkPassword;
+    @NotBlank(message = "필수항목 입니다.", groups = ValidationGroups.NotBlankGroup.class)
+    @Size(min = 10, max = 16, message = "최소 10자, 최대 16자 이하 입력해 주세요.", groups = ValidationGroups.SizeGroup.class)
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*_])[a-zA-Z\\d-~!@#$%^&*_]*$", message = "비밀번호는 특수기호, 영문, 숫자를 모두 포함하여야 합니다.", groups = ValidationGroups.PatternCheckGroup.class)
+    private String passwordChk;
 
-    @NotBlank(message = "이름은 비워둘 수 없습니다.", groups = ValidationGroups.NotBlankGroup.class)
-    @Size(min = 2, max = 5, groups = ValidationGroups.SizeGroup.class)
-    @Pattern(regexp = "^[가-힇]*$", message = "한글만 입력할 수 있습니다.", groups = ValidationGroups.PatternGroup.class)
+    @NotBlank(message = "필수항목 입니다.", groups = ValidationGroups.NotBlankGroup.class)
+    @Size(min = 2, max = 5, message = "최소 2자, 최대 5자 이하 입력해 주세요.", groups = ValidationGroups.SizeGroup.class)
+    @Pattern(regexp = "^[가-힣]*$", message = "이름은 한글만 입력 가능합니다.", groups = ValidationGroups.PatternCheckGroup.class)
     private String name;
 
-    @NotBlank(message = "이메일은 비워둘 수 없습니다.", groups = ValidationGroups.NotBlankGroup.class)
-    @Email
+    // 이메일 형식
+    @NotBlank(message = "필수항목 입니다.", groups = ValidationGroups.NotBlankGroup.class)
+    @Email(message = "이메일을 정확하게 입력해 주세요.", groups = ValidationGroups.PatternCheckGroup.class)
     private String email;
 
-    @NotBlank(message = "휴대폰 번호는 비워둘 수 없습니다.", groups = ValidationGroups.NotBlankGroup.class)
-    @Size(min = 10, max = 11, groups = ValidationGroups.SizeGroup.class)
-    @Pattern(regexp = "^[0-9]*$", message = "숫자만 입력할 수 있습니다.", groups = ValidationGroups.PatternGroup.class)
-    private String cellphone;
-
-    @Size(min = 10, max = 11, groups = ValidationGroups.SizeGroup.class)
-    @Pattern(regexp = "^[0-9]*$", message = "숫자만 입력할 수 있습니다.", groups = ValidationGroups.PatternGroup.class)
     private String phone;
-
-    @NotBlank(message = "주소는 비워둘 수 없습니다.", groups = ValidationGroups.NotBlankGroup.class)
+    private String postCode;
     private String address;
+    private String addressSub;
+
+
+    public User toEntity() {
+        return User.builder()
+                .userName(userName)
+                .password(new BCryptPasswordEncoder().encode(password))
+                .name(name)
+                .email(email)
+                .phone(phone)
+                .postcode(postCode)
+                .address(address)
+                .addressSub(addressSub)
+                .role_id(1)
+                .build();
+    }
 }
