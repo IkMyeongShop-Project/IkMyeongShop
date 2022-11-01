04e0d753760215e4a7533e31bca441efc6be1734
SungHeeCheol <hhhc0213@naver.com>
2022-11-01 22:37:26 +0900

    accountservice 수정

--- src/main/java/com/study/ikmyeongshopteam4/service/AccountService.java
+++ src/main/java/com/study/ikmyeongshopteam4/service/AccountService.java
@@ -4,5 +4,7 @@ import com.study.ikmyeongshopteam4.dto.RegisterReqDto;
 
 public interface AccountService {
 
+    public void duplicateUserName(RegisterReqDto registerReqDto) throws Exception;
+    public void passwordCheck(RegisterReqDto registerReqDto) throws Exception;
     public void register(RegisterReqDto registerReqDto) throws Exception;
 }
