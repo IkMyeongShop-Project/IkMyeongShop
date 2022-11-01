b7e9b5d71f6d5d8c4082d1a4be4599efe04ffc45
SungHeeCheol <hhhc0213@naver.com>
2022-11-01 23:09:41 +0900

    no message

--- src/main/java/com/study/ikmyeongshopteam4/api/adivce/RestControllerExceptionHandler.java
+++ src/main/java/com/study/ikmyeongshopteam4/api/adivce/RestControllerExceptionHandler.java
@@ -1,6 +1,7 @@
-package com.study.ikmyeongshopteam4.api.adivce;
+package com.study.ikmyeongshopteam4.api.advice;
 
 import com.study.ikmyeongshopteam4.dto.CMRespDto;
+import com.study.ikmyeongshopteam4.exception.CustomInternalServerErrorException;
 import com.study.ikmyeongshopteam4.exception.CustomValidationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,8 +13,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class RestControllerExceptionHandler {
 
     @ExceptionHandler(CustomValidationException.class)
-    public ResponseEntity<?> validationErrorException(CustomValidationException e){
-        return ResponseEntity.badRequest().body(new CMRespDto<>(e.getMessage(),e.getErrorMap()));
+    public ResponseEntity<?> validationErrorException(CustomValidationException e) {
+        return ResponseEntity.badRequest().body(new CMRespDto<>(e.getMessage(), e.getErrorMap()));
     }
 
+    @ExceptionHandler(CustomInternalServerErrorException.class)
+    public ResponseEntity<?> internalServerErrorException(CustomInternalServerErrorException e) {
+
+        return ResponseEntity.internalServerError().body(new CMRespDto<>(e.getMessage(), null));
+    }
 }
