4e6594b086b64cf5b28da17888056474af256c28
SungHeeCheol <hhhc0213@naver.com>
2022-11-04 16:34:44 +0900

    기본 로그인 구현 완료 / 구글, 네이버 연결 미완성

--- src/main/java/com/study/ikmyeongshopteam4/config/SecurityConfig.java
+++ src/main/java/com/study/ikmyeongshopteam4/config/SecurityConfig.java
@@ -1,28 +1,37 @@
 package com.study.ikmyeongshopteam4.config;
 
+import com.study.ikmyeongshopteam4.security.AuthFailureHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
         http.httpBasic().disable();
         http.authorizeRequests()
-                .antMatchers("/account/mypage", "/order/cart")
+                .antMatchers("/mypage/index", "/order/cart")
                 .authenticated()
                 .anyRequest()
                 .permitAll()
                 .and()
                 .formLogin()
+                .usernameParameter("username")
                 .loginPage("/account/login")
                 .loginProcessingUrl("/account/login")
+                .failureHandler(new AuthFailureHandler())
                 .defaultSuccessUrl("/index");
     }
 
