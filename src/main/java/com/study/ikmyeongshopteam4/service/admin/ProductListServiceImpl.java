56be312ba8540ee0813dc7248adae28e98692ede
yms213 <dbalstjd789@daum.net>
2022-11-05 16:14:20 +0900

    3

--- src/main/java/com/study/ikmyeongshopteam4/service/admin/ProductListServiceImpl.java
+++ src/main/java/com/study/ikmyeongshopteam4/service/admin/ProductListServiceImpl.java
@@ -1,25 +1,10 @@
-//package com.study.ikmyeongshopteam4.service.admin;
-//
-//import com.study.ikmyeongshopteam4.dto.admin.ProductListDto;
-//import com.study.ikmyeongshopteam4.repository.ProductRepository;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//@RequiredArgsConstructor
-//@Service
-//public class ProductListServiceImpl implements ProductListService{
-//
-//    private final ProductRepository productRepository;
-//
-//    @Override
-//    public List<ProductListDto> getList() throws Exception;
-//    List<ProductListDto> ProductListDtos = new ArrayList<ProductListDto>();
-//
-//        ProductRepository.getList().forEach(ProductList -> {
-//        ProductListDtos.add(ProductList.toDto());
-//    });
-//        return ProductListDtos;
-//}
+package com.study.ikmyeongshopteam4.service.admin;
+
+import com.study.ikmyeongshopteam4.dto.admin.ProductListDto;
+import com.study.ikmyeongshopteam4.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
